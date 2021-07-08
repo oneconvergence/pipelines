678e84a4ba91ece2c65f7d815210df25ef295a51
abikki <ankaiah.bikki@oneconvergence.com>
2021-07-08 16:35:50 +0530

    add contributor api intergration

--- frontend/src/lib/Apis.ts
+++ frontend/src/lib/Apis.ts
@@ -417,7 +417,7 @@ export class Apis {
     body: any
   ): Promise<any> {
 
-    return await this._fetchAndParse<any>(
+    return await this._fetch(
       '/kfam/v1/bindings',
       undefined,
       undefined,
